09c7897568b518bac99182ffaf1d33512f9b96e7
Deibi <121470260+deiviLedesma@users.noreply.github.com>
2025-02-19 20:03:00 -0700

    Add files via upload

---
+++ HOSPITAL.sql
@@ -0,0 +1,76 @@
+create database HOSPITAL;
+use HOSPITAL;
+
+create table pacientes(
+idPaciente int auto_increment primary key,
+nombre varchar(25) not null,
+apellidoPaterno varchar(25) not null,
+apellidoMaterno varchar(25) null,
+fechaNacimiento DATE not null,
+correoElectronico varchar(30) not null unique,
+telefono varchar(15) not null unique,
+contrasenia varchar(125) not null
+);
+
+
+create table direcciones_pacientes(
+idDireccion int auto_increment primary key,
+calle varchar(30) not null,
+colonia varchar(30) not null,
+numero varchar(10) not null,
+idPaciente int not null,
+FOREIGN KEY (idPaciente) REFERENCES pacientes (idPaciente)
+);
+
+
+create table medicos(
+idMedico int auto_increment primary key,
+nombre varchar(25) not null,
+apellidoPaterno varchar(25) not null,
+apellidoMaterno varchar(25) null,
+especialidad varchar(30) not null,
+cedulaProfesional varchar(50) not null unique,
+estado ENUM('ACTIVO', 'INACTIVO') not null default 'ACTIVO',
+contrasenia varchar(125) not null
+);
+
+
+create table horarios_medicos(
+idHorario int auto_increment primary key,
+diaSemana ENUM('Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado', 'Domingo') not null,
+horaInicio DATETIME not null,
+horaFin DATETIME not null,
+idMedico int,
+FOREIGN KEY (idMedico) REFERENCES medicos (idMedico)
+);
+
+
+create table citaMedica(
+idCita int auto_increment primary key,
+tipo ENUM('PROGRAMADA', 'NO PROGRAMADA'),
+fechahora DATETIME not null,
+folio varchar(25) not null unique,
+estado ENUM('PENDIENTE','DISPONIBLE', 'FINALIZADO', 'CANCELADO'), -- pendiente se refiere a que esta asignada pero aun no se atiende
+idPaciente int,
+idMedico int,
+FOREIGN KEY (idPaciente) REFERENCES pacientes (idPaciente),
+FOREIGN KEY (idMedico) REFERENCES medicos (idMedico)
+);
+
+create table consulta(
+idConsulta int auto_increment primary key,
+fechaConsulta DATE not null,
+diagnostico varchar(255) not null,
+tratamiento varchar(255) not null,
+idCita int not null,
+FOREIGN KEY (idCita) REFERENCES citaMedica (idCita)
+);
+
+create table auditoria(
+idAuditoria int auto_increment primary key,
+fechHora DATETIME not null default CURRENT_TIMESTAMP,
+usuarioResponsable int not null, -- da a referencia si el que realiza la accion es el paciente o medico para almacenarlo ahi
+tablaAfectada ENUM('pacientes', 'medicos', 'consulta') not null,
+tipoAccion ENUM('INSERT', 'UPDATE', '"DELETE"') not null
+);
+
