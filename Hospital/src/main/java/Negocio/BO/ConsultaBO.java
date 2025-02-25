75d15eae13535c6acb7ad87de7cb6bd779eac6c9
ramonZamudio5 <ramon.zamudio251924@potros.itson.edu.mx>
2025-02-25 15:22:49 -0700

    Update ConsultaBO.java

--- Hospital/src/main/java/Negocio/BO/ConsultaBO.java
+++ Hospital/src/main/java/Negocio/BO/ConsultaBO.java
@@ -9,5 +9,49 @@ package Negocio.BO;
  * @author SDavidLedesma
  */
 public class ConsultaBO {
-    
+   private final IConsultaDAO consultaDAO;
+    private static final Logger LOG = Logger.getLogger(ConsultaBO.class.getName());
+    private ConsultaMapper mapper = new ConsultaMapper();
+
+    public ConsultaBO(IConexion conexion) {
+        this.consultaDAO = new ConsultaDAO(conexion);
+    }
+
+    public boolean registrarConsulta(ConsultaDTO consulta) throws PersistenciaException, NegocioException {
+        validarConsulta(consulta);
+        Consulta consultaMapper = mapper.toEntity(consulta);
+        try {
+            Consulta ConsultaAgregada = consultaDAO.agregarCOnsulta(consultaMapper);
+            return ConsultaAgregada != null;
+        }catch(PersistenciaException ex){
+            LOG.log(Level.SEVERE, "Error al agendar la cita en la BD", ex);
+            throw new NegocioException("Hubo un error al agendar la cita.", ex);
+        }
+        
+    }
+
+    public Consulta obtenerConsultaPorId(int idConsulta) throws PersistenciaException {
+        if (idConsulta <= 0) {
+            throw new PersistenciaException("El ID de la consulta debe ser mayor a 0.");
+        }
+        return consultaDAO.obtenerConsultaPorId(idConsulta);
+    }
+
+    private void validarConsulta(ConsultaDTO consulta)throws Negocio.Exception.NegocioException, PersistenciaException {
+        if (consulta == null) {
+            throw new PersistenciaException("La consulta no puede ser nula.");
+        }
+        if (consulta.getFechaConsulta() == null || consulta.getFechaConsulta().isAfter(LocalDate.now())) {
+            throw new PersistenciaException("La fecha de la consulta no puede ser futura o nula.");
+        }
+        if (consulta.getDiagnostico() == null || consulta.getDiagnostico().trim().isEmpty()) {
+            throw new PersistenciaException("El diagnóstico no puede estar vacío.");
+        }
+        if (consulta.getTratamiento() == null || consulta.getTratamiento().trim().isEmpty()) {
+           throw new PersistenciaException("El tratamiento no puede estar vacío.");
+        }
+        if (consulta.getIdCita()<= 0) {
+            throw new PersistenciaException("El ID de la cita debe ser válido.");
+        }
+    }
 }
